ac2af2f3511716c42352928dfa3829eda2baf198
Omar Montes <621omontes@csu.fullerton.edu>
2025-11-12 18:36:52 -0800

    Update for multiple clients
    Refactor TCP server implementation into a Server class with client handling and broadcasting features.

--- server.py
+++ server.py
@@ -1,22 +1,52 @@
 import socket   
+from threading import Thread
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('localhost', 9999))
-server.listen(1)
-print("TCP server up and listening on port 9999")
 
-while True:
-    conn, addr = server.accept()
-    data = conn.recv(1024)
-    print(f"Received message: {data.decode()} from {addr}")
-    conn.send(data)
+class Server: 
+    Clients = []
 
-# Simple TCP Echo Server
-# Listens on port 9999 and echoes back any received messages
-# to the sender.
-# To test, run client.py which sends a message to this server
-# and prints the echoed response.
-# To stop the server, interrupt the process (e.g., Ctrl+C).
-# Note: This server runs indefinitely until manually stopped.
-# Make sure to run client.py in a separate terminal or process.
+    def __init__(self, HOST, PORT):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # TCP socket to allow 5 connections maximum.
+        self.socket.bind((HOST, PORT))
+        self.socket.listen(5)
+        print(f"Server started on {HOST}:{PORT}")
 
+
+    def listen(self):
+        while True:
+            client_socket, address = self.socket.accept()
+            print(f"Connection with" + str(address) + " has been established.")
+
+            client_name = client_socket.recv(1024).decode()
+            client = {'client_name': client_name, 'client_socket': client_socket}
+
+            self.broadcast_message(client_name, client_name + " has joined the chat.")
+            self.Clients.append(client)
+            Thread(target = self.handle_new_client, args=(client,)).start()
+
+
+    def handle_new_client(self, client):
+        client_socket = client['client_socket']
+        client_name = client['client_name']
+
+        while True:
+                client_message = client_socket.recv(1024).decode()
+                if client_message.strip() == client_name + ": bye" or not client_message.strip():
+                    self.broadcast_message(client_name, client_name + " has left the chat.")
+                    Server.Clients.remove(client)
+                    client_socket.close()
+                    break
+                else:
+                    self.broadcast_message(client_name, client_message)
+
+    def broadcast_message(self, sender_name, message):
+        for client in self.Clients:
+            client_socket = client['client_socket']
+            client_name = client['client_name']
+            if client_name != sender_name:
+                client_socket.send(message.encode())
+
+
+if __name__ == "__main__":
+    server = Server('localhost', 12345)
+    server.listen()
